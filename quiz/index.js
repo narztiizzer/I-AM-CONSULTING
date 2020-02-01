2ab14d4db758832ec8a28f80fc72d9ea7c3af3e1
narztiizzer <nattapong.poom@gmail.com>
2020-02-01 13:37:46 +0700

    [ADDED] palindrome quiz

--- quiz/index.js
+++ quiz/index.js
@@ -1,8 +1,6 @@
 module.exports.palindrome = function(request, response){
-    var sample = "meem".toLowerCase()
-    
-    var sampleLength = sample.length / 2
-    var firstPart = sample.substring(0, sampleLength)
+    var sample = "deleveled".toLowerCase()
+    var firstPart = sample.substring(0, (sample.length / 2))
     var isPalindrome = true
 
     for(var index = 0; index < firstPart.length; index++){
@@ -10,9 +8,7 @@ module.exports.palindrome = function(request, response){
         var firstPartChar = firstPart.charAt(index)
         var secondPartChar = sample.charAt(secondPartIndex)
 
-        if(firstPartChar !== secondPartChar){
-            isPalindrome = false
-        }
+        if(firstPartChar !== secondPartChar){isPalindrome = false }
     }
 
     response.json({
